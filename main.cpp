83660cd7d4d9116c14384f10298e90b7a50f5a91
Clément P <penicaut.clement@gmail.com>
2024-06-14 22:59:01 +0200

    Chapter Two : Vector3 structure

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "sdltemplate.h"
+#include "vec3.h"
 
 int main(){
 	int width = 800;
@@ -9,12 +10,10 @@ int main(){
 	sdltemplate::loop();
 	for(int y = height-1; y >= 0; y--){
 		for(int x = 0; x < width; x++){
-			float r = float(x) / float(width);
-			float g = float(y) / float(height);
-			float b = 0.2;
-			int ir = int(255.99*r);
-			int ig = int(255.99*g);
-			int ib = int(255.99*b);
+			vec3 col(float(x) / float(width), float(y) / float(height),0.2);
+			int ir = int(255.99*col[0]);
+			int ig = int(255.99*col[1]);
+			int ib = int(255.99*col[2]);
 			//std::cout << ir << " " << ig << " " << ib << "\n";
 			sdltemplate::setDrawColor(sdltemplate::createColor(ir, ig, ib, 255));
 			sdltemplate::drawPoint(x,height - 1 - y);
