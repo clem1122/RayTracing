e706b932b061d5512ca56900f56b4f7850ad387d
Clément P <penicaut.clement@gmail.com>
2024-06-14 23:22:09 +0200

    Chapter Three : Ray structure

--- vec3.h
+++ vec3.h
@@ -78,7 +78,7 @@ inline vec3 operator/(const vec3 &v1, const vec3 &v2) {
 	return vec3(v1.e[0] / v2.e[0], v1.e[1] / v2.e[1], v1.e[2] / v2.e[2]);
 }
 
-inline vec3 operator*(const vec3 &v1, const float t) {
+inline vec3 operator*(const float t, const vec3 &v1) {
 	return vec3(v1.e[0] * t, v1.e[1] * t, v1.e[2] * t);
 }
 
